9fc50890bf4c8ab4d1afa46fa75c21011d1f72c8
Anand.C <122742595+AnandC7github@users.noreply.github.com>
2023-03-11 23:56:33 +0530

    Add files via upload

---
+++ CircleAr.java
@@ -0,0 +1,29 @@
+import java.io.*;
+class test
+{
+    public void area()
+    {
+        try
+        {
+            DataInputStream dis=new DataInputStream(System.in);
+            float r,a;
+            System.out.println("Enter the radius:");
+            r=Float.parseFloat(dis.readLine());
+            a=3.14152857f*r*r;
+            System.out.println("Area of the circle: "+a);
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+    }
+    
+}
+public class CircleAr
+{
+    public static void main(String[] args)
+	{
+        test t=new test();
+        t.area();
+    }
+}
